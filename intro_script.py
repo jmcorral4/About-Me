bc6d8217b4da690c2ef906f398d5f227ae24efac
Jose Corral <josemcorral4@outlook.com>
2025-05-30 17:04:36 -0500

    Update intro_script.py

--- intro_script.py
+++ intro_script.py
@@ -4,78 +4,23 @@ color = "red"
 sport = "basketball, soccer, and F1."
 team = "Manchester United"  # soccer
 team2 = "Philadelphia 76ers"  # basketball
-team3 = Carlos Sainz Jr.  # F1 driver
-Career_goal = "to become a cybersecurity engineer and delve into AI and machine learning"
-
-print("Hello, my name is", name, "and I am", age, "years old.")
+team3 = "Carlos Sainz Jr."  # F1 driver
+career_goal = "become a cybersecurity engineer and delve into AI and machine learning"
 
 def greet(name):
     print("Hey", name, "welcome to the Python world!")
 
 greet("Jose")
 
-def favorite_things(color, sport):
-    print("My favorite color is", color, "and my favorite sports are", sport)
+print("Hello, my name is", name, "and I am", age, "years old.")
 
-print("I support", team, "in soccer and", team2, "in basketball.")
+def favorite_color(color):
+    print("My favorite color is", color)
+
+favorite_color(color)
+
+print("I support", team, "in soccer and", team2, "in basketball!")
 
 print("I admire", team3, "in F1 racing.")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("I will", career_goal, "in the future with enough experience.")
