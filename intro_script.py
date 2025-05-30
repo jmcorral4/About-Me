198b290325610614d6d2195f1f30d07e132f354f
Jose Corral <josemcorral4@outlook.com>
2025-05-30 16:57:41 -0500

    Add files via upload

---
+++ intro_script.py
@@ -0,0 +1,81 @@
+name = "Jose"
+age = 28
+color = "red"
+sport = "basketball, soccer, and F1."
+team = "Manchester United"  # soccer
+team2 = "Philadelphia 76ers"  # basketball
+team3 = Carlos Sainz Jr.  # F1 driver
+Career_goal = "to become a cybersecurity engineer and delve into AI and machine learning"
+
+print("Hello, my name is", name, "and I am", age, "years old.")
+
+def greet(name):
+    print("Hey", name, "welcome to the Python world!")
+
+greet("Jose")
+
+def favorite_things(color, sport):
+    print("My favorite color is", color, "and my favorite sports are", sport)
+
+print("I support", team, "in soccer and", team2, "in basketball.")
+
+print("I admire", team3, "in F1 racing.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
